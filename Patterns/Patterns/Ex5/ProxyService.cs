5e4a34b0244ca8c519f09a8d348960d4d2dff0f8
Laska-xxx <ZubavichusEYa23@st.ithub.ru>
2025-10-29 18:35:24 +0300

    Update ProxyService.cs

--- Patterns/Patterns/Ex5/ProxyService.cs
+++ Patterns/Patterns/Ex5/ProxyService.cs
@@ -2,6 +2,13 @@
 {
     internal class ProxyService : IService
     {
+        private RealService _realService;
+
+        public ProxyService(RealService realService)
+        {
+            _realService = realService;
+        }
+
         public string GetData(string request)
         {
             throw new NotImplementedException();
